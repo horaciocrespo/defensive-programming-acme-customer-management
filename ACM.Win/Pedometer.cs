49d040408c630c9bd38a3717e688aecf495132f0
Jhonny Horacio Crespo <jhonny.h.cr@gmail.com>
2018-04-22 00:52:07 -0400

    Add win form for pedometer

---
+++ ACM.Win/Pedometer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ACM.BL;
+
+namespace ACM.Win
+{
+    public partial class Pedometer : Form
+    {
+        public Pedometer()
+        {
+            InitializeComponent();
+        }
+
+  
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
