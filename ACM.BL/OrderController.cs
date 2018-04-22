e12eaa5976c92feb86f213eb0c28fa5f28a5399a
Jhonny Horacio Crespo <jhonny.h.cr@gmail.com>
2018-04-21 23:41:17 -0400

    Refactor the order related process into a new class OrderController

---
+++ ACM.BL/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using Core.Common;
+
+namespace ACM.BL
+{
+    public class OrderController
+    {
+        // flags usually go at the end
+        public void PlaceOrder(Customer customer,
+                                Order order,
+                                Payment payment,
+                                bool allowSplitOrders,
+                                bool emailReceipt)
+        {
+            var customerRepository = new CustomerRepository();
+            customerRepository.Add(customer);
+
+            var orderRepository = new OrderRepository();
+            orderRepository.Add(order);
+
+            var inventoryRepository = new InventoryRepository();
+            inventoryRepository.OrderItems(order, allowSplitOrders);
+
+            payment.ProcessPayment();
+
+            if (emailReceipt)
+            {
+                customer.ValidateEmail();
+                customerRepository.Update();
+
+                var emailLibrary = new EmailLibrary();
+                emailLibrary.SendEmail(customer.EmailAddress, "Here is your receipt");
+            }
+        }
+    }
+}
--- AcmeCustomerManagement/Program.cs
+++ AcmeCustomerManagement/Program.cs
@@ -15,9 +15,6 @@ namespace AcmeCustomerManagement
             var customer = new Customer();
             // populate the customer instance
 
-            var customerRepository = new CustomerRepository();
-            customerRepository.Add(customer);
-
             var order = new Order();
             // populate the order instance
 
@@ -28,22 +25,8 @@ namespace AcmeCustomerManagement
             var payment = new Payment();
             // populate the payment info from the UI
 
-            var orderRepository = new OrderRepository();
-            orderRepository.Add(order);
-
-            var inventoryRepository = new InventoryRepository();
-            inventoryRepository.OrderItems(order, allowSplitOrders);
-
-            payment.ProcessPayment();
-
-            if (emailReceipt)
-            {
-                customer.ValidateEmail();
-                customerRepository.Update();
-
-                var emailLibrary = new EmailLibrary();
-                emailLibrary.SendEmail(customer.EmailAddress, "Here is your receipt");
-            }
+            var orderController = new OrderController();
+            orderController.PlaceOrder(customer, order, payment, allowSplitOrders, emailReceipt);
         }
     }
 }
