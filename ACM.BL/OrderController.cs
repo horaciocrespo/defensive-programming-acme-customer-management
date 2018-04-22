3e620e3a68caf41733bac6b7c3583e07c90f80f9
Jhonny Horacio Crespo <jhonny.h.cr@gmail.com>
2018-04-21 23:57:45 -0400

    Add review of the PlaceOrder method inside the OrderController class

--- ACM.BL/OrderController.cs
+++ ACM.BL/OrderController.cs
@@ -2,6 +2,17 @@
 
 namespace ACM.BL
 {
+    /// Check that this method agains the characteristic of a clean,
+    /// testable and predictable method:
+    /// 
+    /// Does it have a clear purpose? Yes, its purpose is to place an order
+    /// Does it have a good name? Yes
+    /// Is all of its code focused on the goal? Yes, all of this code is required
+    /// in order to meet the requirements of placing an order.
+    /// Does it have a reasonable size? Yes, it fits nicely in the screen.
+    /// Can be tested? Yes, this method is now in a class library.
+    /// Does it have a predictable result? Well, this method does not return a result
+    /// nor does it have any error or exception handling. (@todo)
     public class OrderController
     {
         // flags usually go at the end
